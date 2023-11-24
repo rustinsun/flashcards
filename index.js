b0f92a545d66e04fd068c20c2f15a2e378f24b8a
@rustinsun <dantalian696@gmail.com>
2023-11-24 14:38:44 +0300

    some

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-// comment
-// comment 2
+// const fs = require('fs').promises;
+// const { EOL } = require('os');
+// const { questionObj, questionObj2 } = require('./QuestionClass');
+
+// class Model {
+//   constructor(question, answer) {
+//     this.question = question
+//     this.answer = answer
+//   }
+
+//   getQuestion(questionObj) {
+
+//   }
+
+// }
+
+// const vi
+
+// module.exports = Model;
